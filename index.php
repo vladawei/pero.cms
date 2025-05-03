4554da183001fba1afae631b34efebe3b163d4d3
GakmanAlexey <gakman@ya.ru>
2025-05-03 20:35:06 +0300

    Добавлены конфиги для nginx и апача

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+define('APP_ROOT', __DIR__);
+define('DS', DIRECTORY_SEPARATOR); 
+require_once APP_ROOT.DS."Modules".DS."Core".DS."Modul".DS."Core";
+$Core = new \Modules\Core\Modul\Core;
+
+?>
